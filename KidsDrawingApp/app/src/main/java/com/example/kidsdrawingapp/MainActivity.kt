a8b4fa05b7ede7290767cd69f018c3e77d110b71
Kundan Kumar <kundan211097@gmail.com>
2021-04-29 01:14:45 +0530

    Save and share the drawing

--- KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -5,6 +5,11 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.media.MediaScannerConnection
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -16,6 +21,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint: ImageButton? = null
@@ -32,32 +40,45 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeChooseDialog()
         }
         ib_gallery.setOnClickListener {
-            if(isReadStorageAllowed()){
+            if (isReadStorageAllowed()) {
                 //run  our code to get the image from gallery
-                val pickPhotoIntent = Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val pickPhotoIntent = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
                 startActivityForResult(pickPhotoIntent, GALLERY)
-            }else{
+            } else {
                 requestStoragePermission()
             }
         }
         ib_undo.setOnClickListener {
             drawing_View.onClickUndo()
         }
+        ib_save.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_View_container)).execute()
+            } else {
+                requestStoragePermission()
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == GALLERY){
-                try{
-                    if(data!!.data !=null){
-                        iv_background.visibility =View.VISIBLE
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                try {
+                    if (data!!.data != null) {
+                        iv_background.visibility = View.VISIBLE
                         iv_background.setImageURI(data.data)
-                    }else{
-                        Toast.makeText(this, "Error in parsing image or its corrupted.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Error in parsing image or its corrupted.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
-                }catch (e:Exception){
+                } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }
@@ -130,19 +151,115 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == STORAGE_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted now you can read the storage file ", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, "Oops you just denied the permission.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Permission granted now you can read the storage file ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(this, "Oops you just denied the permission.", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
-    private fun isReadStorageAllowed():Boolean{
-        val result=ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
-        return result ==PackageManager.PERMISSION_GRANTED
+
+    private fun isReadStorageAllowed(): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
     }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(
+            view.width,
+            view.height, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return returnedBitmap
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap) :
+        AsyncTask<Any, Void, String>() {
+        private lateinit var mProgressDialog: Dialog
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result = ""
+            if (mBitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val f = File(
+                        externalCacheDir!!.absoluteFile.toString() +
+                                File.separator + "KidDrawingApp_" + System.currentTimeMillis() / 1000 +
+                                ",png"
+                    )
+                    val fos = FileOutputStream(f)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result = f.absolutePath
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            if (!result!!.isEmpty()) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "File saved successfully :$result",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong while saving the file.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            MediaScannerConnection.scanFile(this@MainActivity,
+                arrayOf(result),null){
+                path, uri ->  val shareIntent=Intent()
+                shareIntent.action=Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM,uri)
+                shareIntent.type="image/png"
+                startActivity(
+                    Intent.createChooser(
+                        shareIntent,"Share"
+                    )
+                )
+            }
+        }
+
+        private fun showProgressDialog() {
+            mProgressDialog = Dialog(this@MainActivity)
+            mProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            mProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog() {
+            mProgressDialog.cancel()
+        }
+    }
+
 
     companion object {
         private const val STORAGE_PERMISSION_CODE = 1
-        private const val GALLERY=1
+        private const val GALLERY = 1
     }
 }
